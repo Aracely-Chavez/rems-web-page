0e2717a9bf9b6944931470840d17b37842f9b5d5
Aracely-Chavez <hml101299@gmail.com>
2024-05-06 22:17:51 -0500

    Act New Pages

---
+++ src/app/contratos/page.tsx
@@ -0,0 +1,120 @@
+// @ts-nocheck
+"use client";
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const ContratoCard = ({ contrato, isChecked, onToggle }) => {
+  const { id, razon_social, fecha_inicio } = contrato;
+
+  const handleToggle = () => {
+    onToggle(id);
+  };
+
+  return (
+    <div className="border rounded-lg shadow-lg p-4 bg-white flex justify-between items-center">
+        <div>
+            <h3 className="text-xl font-semibold mb-2">{razon_social}</h3>
+            <p className="text-gray-600">Fecha de inicio: {fecha_inicio}</p>
+            {/* Otros datos del contrato */}
+            {/* ... */}
+        </div>
+        <div className="ml-4">
+            <label className="inline-flex items-center">
+            <input
+                type="checkbox"
+                className="form-checkbox h-6 w-6 text-indigo-600"
+                checked={isChecked}
+                onChange={handleToggle}
+            />
+            <span className="ml-2 text-gray-700">Seleccionar</span>
+            </label>
+        </div>
+        </div>
+  );
+};
+
+const ContratosScreen = ( ) => {
+  const [contratos, setContratos] = useState<[]>([]);
+  const [selectedContratos, setSelectedContratos] = useState([]);
+  const [error, setError] = useState(null);
+  const [cargando, setCargando] = useState(true);
+
+  const handleEnviarSeleccion = () => {
+    // Aquí puedes enviar los IDs de los contratos seleccionados a la API
+    console.log('Contratos seleccionados:', selectedContratos);
+    // Lógica para enviar los IDs seleccionados a la API
+
+    // Obtener instancia del enrutador de Next.js
+    const router = useRouter();
+
+    // Convertir los contratos seleccionados a una cadena de query string
+    const queryString = selectedContratos.join(',');
+
+    // Navegar a la página FechasPago con los contratos seleccionados como query string
+    router.push(`/fechasPago?contratos=${queryString}`);
+  };
+
+  useEffect(() => {
+    //const url = 'http://164.68.101.193:5000/calcular_pagos';
+    const url = 'http://127.0.0.1:5000/contratos';
+
+    const fetchData = async () => {
+      try {
+        const requestOptions = {
+          method: 'GET',
+        };
+        const response = await fetch(url, requestOptions);
+        if (!response.ok) {
+          throw new Error('Error al obtener los datos');
+        }
+        const data = await response.json();
+        setContratos(data);
+        setCargando(false);
+      } catch (error) {
+        setError(error.message);
+        setCargando(false);
+      }
+    };
+
+    fetchData();
+
+  }, []);
+
+  const handleToggleContrato = (id) => {
+    setSelectedContratos((prevSelected) =>
+      prevSelected.includes(id) ? prevSelected.filter((selectedId) => selectedId !== id) : [...prevSelected, id]
+    );
+  };
+
+
+  return (
+    <main className="flex relative w-full justify-center font-montserrat mb-7">
+      <img src="/HeaderBackground.svg" alt="bg" className="absolute w-[100%] xl:-top-24 min-h-[400px] -top-6 -z-10" />
+      <div className="container px-8">
+        <div className="flex h-[280px] sm:h-[330px] md:h-[360px] lg:h-[410px] 2xl:h-[520px]  justify-center items-center">
+          <h1 className="font-bold text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl">Fechas de pago</h1>
+        </div>
+            <div>
+            <h1>Contratos</h1>
+            <div className="contrato-container my-8">
+                {contratos.map((contrato) => (
+                <ContratoCard
+                    key={contrato.id}
+                    contrato={contrato}
+                    isChecked={selectedContratos.includes(contrato.id)}
+                    onToggle={handleToggleContrato}
+                />
+                ))}
+            </div>
+            <Link href={{ pathname: '/fechasPago', query: { contratos: selectedContratos.join(',') } }}
+                className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Ir a Fechas de Pago
+            </Link>
+            </div>
+        </div>
+    </main>
+  );
+};
+
+export default ContratosScreen;
---
+++ src/app/fechasPago/page.tsx
@@ -0,0 +1,123 @@
+// @ts-nocheck
+"use client";
+
+import React, { useEffect, useState } from 'react';
+
+import { useSearchParams } from 'next/navigation'
+
+
+export default function FechasPago() {
+    const searchParams = useSearchParams();
+  
+    const search = searchParams.get('contratos');
+
+    const [contratos, setContratos] = useState<[]>([]);
+    const [fechas, setFechas] = useState<string[]>([]);
+    const [cargando, setCargando] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        //const url = 'http://164.68.101.193:5000/calcular_pagos';
+        const url = 'http://127.0.0.1:5000/contratos_por_ids?ids=' + search;
+
+        const fetchData = async () => {
+          try {
+            const requestOptions = {
+              method: 'GET',
+            };
+            const response = await fetch(url, requestOptions);
+            if (!response.ok) {
+              throw new Error('Error al obtener los datos');
+            }
+            const data = await response.json();
+            setContratos(data);
+            setCargando(false);
+          } catch (error) {
+            setError(error.message);
+            setCargando(false);
+          }
+        };
+    
+        fetchData();
+    
+      }, []);
+
+
+      function startOfMonth(date)
+        {
+            // Create a new Date object representing the start of the month by using the year and month of the input date and setting the day to 1
+            return new Date(date.getFullYear(), date.getMonth(), 1);
+        }
+
+      useEffect(() => {
+        
+        let fechasSet: Set<string> = new Set();
+        contratos.forEach(contrato => {
+        contrato.pagos.forEach(pago => {
+            fechasSet.add(pago.fecha);
+        });
+        });
+
+        // Convertir Set a Array y ordenar las fechas
+        const fechasOrdenadas = Array.from(fechasSet).sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+
+        // Generar todas las fechas entre la más antigua y la más reciente, solo el primer día de cada mes
+        const fechaInicio = new Date(fechasOrdenadas[0]);
+        const fechaFin = new Date(fechasOrdenadas[fechasOrdenadas.length - 1]);
+
+        const fechasIntermedias: string[] = [];
+        let fecha = new Date(fechaInicio);
+        while (fecha <= fechaFin) {
+            fechasIntermedias.push(new Date(fecha).toISOString().split('T')[0]);
+            fecha.setDate(fecha.getDate() + 32); // Aseguramos que siempre sea el primer día del mes
+            fecha=startOfMonth(fecha);
+        }
+
+        // Establecer todas las fechas en el estado
+        setFechas(fechasIntermedias);
+      }, [contratos]);
+  
+    if (cargando) {
+    return <div>Cargando...</div>;
+    }
+
+  return (
+    <main className="flex relative w-full justify-center font-montserrat mb-7">
+      <img src="/HeaderBackground.svg" alt="bg" className="absolute w-[100%] xl:-top-24 min-h-[400px] -top-6 -z-10" />
+      <div className="container px-8">
+        <div className="flex h-[280px] sm:h-[330px] md:h-[360px] lg:h-[410px] 2xl:h-[520px]  justify-center items-center">
+          <h1 className="font-bold text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl">Fechas de pago</h1>
+        </div>
+        <div className ="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className ="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            <div className ="overflow-hidden">
+                <table className='min-w-full text-left text-sm font-light text-surface dark:text-white'>
+                    <thead className="border-b border-neutral-200 font-medium dark:border-white/10">
+                        <tr>
+                        <th className="px-6 py-4">Contrato</th>
+                        {fechas.map(fecha => (
+                            <th key={fecha} className="px-6 py-4 whitespace-nowrap">{fecha}</th>
+                        ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {contratos.map(contrato => (
+                        <tr key={contrato.id} className="border-b border-neutral-200 dark:border-white/10">
+                            <td className="whitespace-nowrap px-6 py-4">{contrato.nombre}</td>
+                            {fechas.map(fecha => {
+                            const pago = contrato.pagos.find(p => p.fecha === fecha);
+                            return <td key={fecha} className="whitespace-nowrap px-6 py-4">{pago ? pago.monto : '-'}</td>;
+                            })}
+                        </tr>
+                        ))}
+                    </tbody>
+                    </table>
+                </div>
+            </div>
+            </div>
+      </div>
+
+    </main>
+  );
+}
