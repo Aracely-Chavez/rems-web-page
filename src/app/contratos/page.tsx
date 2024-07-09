8da653c725d6cbdbde99ea68c082975d5ebd241b
Aracely Chavez <hml101299@gmail.com>
2024-07-09 18:27:55 -0500

    Act

--- src/app/contratos/page.tsx
+++ src/app/contratos/page.tsx
@@ -103,6 +103,35 @@ const ContratosScreen = ( ) => {
     return 0;
   });
 
+  const deletePagos = () => {
+    const url = 'http://164.68.101.193:5000/eliminar_contratos';
+    //const url = 'http://127.0.0.1:5000/guardar_pagos';
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(selectedContratos)
+    };
+
+    fetch(url, requestOptions)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error en la respuesta de la solicitud de guardado');
+        }
+        return response.json(); // o response.text() dependiendo de la respuesta esperada
+    })
+    .then(data => {
+        alert('Eliminación correcta');
+        window.location.reload();
+        // Puedes agregar aquí cualquier otra lógica que necesites realizar con los datos
+    })
+    .catch(error => {
+        console.error('Error al realizar la solicitud de guardado:', error);
+        alert('Hubo un error al realizar la solicitud de eliminación');
+    });
+  };
+
   return (
     <main className="flex relative w-full justify-center font-montserrat mb-7">
       <img src="/HeaderBackground.svg" alt="bg" className="absolute w-[100%] xl:-top-24 min-h-[400px] -top-6 -z-10" />
@@ -135,10 +164,13 @@ const ContratosScreen = ( ) => {
                 />
                 ))}
             </div>
-            <Link href={{ pathname: '/fechasPago', query: { contratos: selectedContratos.join(',') } }}
-                className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Ir a Fechas de Pago
-            </Link>
+              <div className='flex justify-between'>
+                <Link href={{ pathname: '/fechasPago', query: { contratos: selectedContratos.join(',') } }}
+                    className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Ir a Fechas de Pago
+                </Link>
+                <button onClick={() => deletePagos()} type="button" className="w-36 text-white bg-red-700 py-2 px-4 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Eliminar</button>
+              </div>
             </div>
         </div>
     </main>
